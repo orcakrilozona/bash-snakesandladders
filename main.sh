aa70f2739f7d1396d3cfd10b06396ea9773ae2e9
root <root@localhost.localdomain>
2016-05-18 20:53:54 +0630

    Some mod

--- main.sh
+++ main.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Snakes and ladders game
+# Written by Yan Naing Myint ( The Cyber Wings Team )
+# Contact: 09799950510
+# Email: yannaing@cyberwings.asia
+# Reference Picture: http://www.codewithc.com/wp-content/uploads/2014/05/snakes-and-ladders.jpg
+# Or reference picture is also in repository https://github.com/orcakrilozona/bash-snakesandladders/blob/master/reference-pic.jpg
+
 player1=1
 squares=100
 
